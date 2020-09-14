b915480d521bb88c2a83215f12c272bc174a9479
martinviiberg <karl-martin.viiberg@khk.ee>
2020-09-14 07:27:55 -0400

    lisa_kasutaja.sh

---
+++ lisa_kasutaja.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#kasutaja lisamise skript
+#kasutaja nimi tuleb anda skripti parameetrina
+#skript peab antud nimega kasutajat lisama systeemi ja pakkuma sobiliku shelli
+#
+#kasurea kaudu parameetrite arv on $# sees olev number,
+#meie korral peab olema 1 parameeter - kasutajanimi
+#kui aga antud parameeter ei ole lisatud - tuleb juhendada kasutajat, kuidas skriptiga tood teha
+if [ $# -ne 1 ]; then
+	echo "Kasutusjuhend: $0 kasutajanimi"
+else
+	#defineerime vajalik muutuja kasutajanime salvestamiseks
+	#kuna andmed edastataksee kasurealt skripti parameetrina, siis need on $1 ja $2 jne
+	#kasutajanimi on esimene parameeter, seega tuleb kasutada $1 muutujat
+	kasutajanimi=$1
+	#kasutame kasutaja lisamiseks k2sk vajalike v6tmetega
+	useradd $kasutajanimi -m -s /bin/bash
+	if [ $? -eq 0 ]; then
+	echo "Kasutaja nimega $kasutajanimi on lisatud systeemi."
+	fi
+fi
+
